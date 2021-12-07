dc3187daa26bc242799b0fbfda5b46be5b160d88
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2021-12-08 00:21:24 +0300

    added.

---
+++ Burs/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Ogrenciler ogrenciler = new Ogrenciler();
+        BursluOgrenciler burs = new BursluOgrenciler();
+
+        ogrenciler.DersUcretiHesapla();
+        burs.DersUcretiHesapla();
+    }
+}
